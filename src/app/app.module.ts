09c222d7b478a6431783a92c30f656ba3c5cbec8
Eugênio Francisco da Silva <eugeniofr17@gmail.com>
2023-10-16 16:23:34 -0300

    Created all structure and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { InputAddItensComponent } from './components/input-add-itens/input-add-itens.component';
+import { DeleteAllItensComponent } from './components/delete-all-itens/delete-all-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    InputAddItensComponent,
+    DeleteAllItensComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/delete-all-itens/delete-all-itens.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-all-itens',
+  templateUrl: './delete-all-itens.component.html',
+  styleUrls: ['./delete-all-itens.component.css']
+})
+export class DeleteAllItensComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
